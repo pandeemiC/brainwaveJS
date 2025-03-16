661c25d2bbb539351bfbbb16484c0b3127f0428e
pandeemiC <lkooxer55@hotmail.com>
2025-03-16 13:56:19 +0300

    header commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
-function App() {
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-       Hello World!
-      </h1>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+    </div>  
+    <ButtonGradient />
+
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import Section from './Section'
+
+const Hero = () => {
+  return (
+    <div>
+      Hero
+    </div>
+  )
+}
+
+export default Hero
