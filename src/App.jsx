ee26752bba861a14a58408ec20845a0e4bb550f8
pandeemiC <lkooxer55@hotmail.com>
2025-03-18 14:20:33 +0300

    services done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-
-import ButtonGradient from './assets/svg/ButtonGradient';
-import Button from './components/Button';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Benefits from './components/Benefits';
-import Collaboration from './components/Collaboration';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 const App = () => {
-
   return (
     <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
-      <Hero />
-      <Benefits />
-      <Collaboration />
-    </div>  
-    <ButtonGradient />
-
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+      </div>
+      <ButtonGradient />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
