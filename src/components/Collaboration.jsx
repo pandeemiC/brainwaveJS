a5206025b3aff1ab5034b2107f28649e7851a66a
pandeemiC <lkooxer55@hotmail.com>
2025-03-18 12:41:08 +0300

    updated collaboration

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -30,13 +30,17 @@ const Collaboration = () => {
           <Button>Try It Now</Button>
         </div>
 
-        <div className="lg:ml-auto xl:w-[23rem] mt-10">
+        <div className="lg:ml-auto xl:w-[38rem] mt-4">
           <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">{collabText2}</p>
-          <div className="relative l-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale-75 md:scale-100">
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale-75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
                   <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                     <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
-                      <img src={brainwaveSymbol} width={48} height={48} alt="brainwave" />
+                      <img 
+                        src={brainwaveSymbol} 
+                        width={48} 
+                        height={48} 
+                        alt="brainwave" />
                     </div>
                   </div>
             </div>
