821574206edb71314a6ba6c0e561bd35088efc79
pandeemiC <lkooxer55@hotmail.com>
2025-03-19 15:22:34 +0300

    roadmap finished

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Section from "./Section";
+import Heading from "./Heading";
+import TagLine from "./TagLine";
+import { GradientLight } from "./design/Benefits";
+import { roadmap } from "../constants";
+import { grid, check2, loading1 } from "../assets";
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
+        <Heading
+          tag="Ready to get started"
+          title="What We Are Working On"
+          className="text-center"
+        />
+        <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+          {roadmap.map((item) => {
+            const status = item.status === "done" ? "Done" : "In Progress";
+
+            return (
+              <div
+                key={item.id}
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                  item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                }`}
+              >
+                <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                  {/* Conditionally render GradientLight for odd IDs */}
+                  {item.id % 2 !== 0 && <GradientLight />}
+                  <div className="absolute top-0 left-0 max-w-full">
+                    <img
+                      src={grid}
+                      className="w-full"
+                      width={550}
+                      height={550}
+                      alt="Grid"
+                    />
+                  </div>
+                  <div className="relative z-1">
+                    <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
+                      <TagLine>{item.date}</TagLine>
+                      <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
+                        <img
+                          src={item.status === "done" ? check2 : loading1}
+                          alt="Status"
+                          className="mr-2.5"
+                          width={16}
+                          height={16}
+                        />
+                        <div className="tagline">{status}</div>
+                      </div>
+                    </div>
+                    <div className="mb-10 -my-10 -mx-15">
+                      <img
+                        src={item.imageUrl}
+                        className="w-full"
+                        width={630}
+                        height={420}
+                        alt={item.title}
+                      />
+                    </div>
+                    <h4 className="h4 mb-4">{item.title}</h4>
+                    <p className="body-2 text-n-4">{item.text}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Roadmap;
